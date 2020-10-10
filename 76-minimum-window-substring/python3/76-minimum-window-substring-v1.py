4bc8bce4d7acab8eabedf16f69075f0a17eddf27
Andrew Maraev <andrey.maraev@outlook.com>
2020-10-09 22:19:46 -0400

    Optimize solution for the problem #76 using a set

--- 76-minimum-window-substring/python3/76-minimum-window-substring-v1.py
+++ 76-minimum-window-substring/python3/76-minimum-window-substring-v1.py
@@ -5,6 +5,7 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         counter = dict()
         seen = dict()
+        expecting = set(t)
         for v in t:
             seen[v] = 0
             if v in counter:
@@ -15,30 +16,28 @@ class Solution:
         left = right = 0
         window = (left, len(s))
         while right < len(s):
-            while not self.isDesirable(seen, counter) and right < len(s):
+            while len(expecting) > 0 and right < len(s):
                 r = s[right]
                 if r in counter:
                     seen[r] += 1
+                    if r in expecting and seen[r] >= counter[r]:
+                        expecting.remove(r)
                 right += 1
-            while self.isDesirable(seen, counter):
+            while len(expecting) == 0:
                 (wl, wr) = window
                 if (right - left) < (wr - wl):
                     window = (left, right)
                 l = s[left]
                 if l in seen:
                     seen[l] -= 1
+                    if l not in expecting and seen[l] < counter[l]:
+                        expecting.add(l)
                 left += 1
         if window == (left, len(s)):
             return ""
         else:
             (wl, wr) = window
-            return s[wl:wr]
-                
-    def isDesirable(self, actual, expected):
-        for k, v in expected.items():
-            if v > actual[k]:
-                return False
-        return True            
+            return s[wl:wr]        
 
 def test(expected, *input):
     start = time.time_ns()
