fd091b84c017904245f5f139d6e17ab230a2f86e
Andrew Maraev <andrey.maraev@outlook.com>
2020-11-17 20:07:34 -0500

    Add solution to the problem 1367

---
+++ 1367-linked-list-in-binary-tree/csharp/1367-linked-list-in-binary-tree-v1.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSubPath(ListNode head, TreeNode root) {
+        Func<ListNode, TreeNode, bool> dfs = null;
+        dfs = (h,r) => {
+            if (h == null) return true;
+            if (r == null) return false;
+            return r.val == h.val && (dfs(h.next, r.left) || dfs(h.next, r.right));
+        };
+        
+        if (head == null) return true;
+        if (root == null) return false;
+        
+        return dfs(head, root) || IsSubPath(head, root.left) || IsSubPath(head, root.right);
+    }
+}
+
+public static class Program
+{
+    public static void Main()   
+    {
+        Test("aacaaca", "aacaacaaca");
+        Test("leet", "leetcodeleet");
+    }
+
+    private static void Test(string expected, string s)
+    {
+        var solution = new Solution();
+        var stopwatch = Stopwatch.StartNew();
+        var actual = solution.LongestPrefix(s);
+        if (actual != expected)
+        {
+            Console.WriteLine($"actual value '{actual}' is not equal to expected value '{expected}'");
+        }
+        stopwatch.Stop();
+        var elapsed = stopwatch.Elapsed.TotalSeconds;
+        Console.WriteLine($"elapsed: {elapsed} secs");
+    }
+
+    private static TreeNode ReadTreeNodeInput(int?[] input)
+    {
+        TreeNode root = null;
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        var i = 0;
+        while (i < input.Length)        
+        {
+            if (root == null)
+            {
+                root = new TreeNode(input[i].Value);
+                queue.Enqueue(root);
+                i++;
+            }
+            else
+            {
+                var node = queue.Dequeue();
+                var left = input[i];
+                var right = input[i+1];
+                i += 2;
+                if (left.HasValue)
+                {
+                    node.left = new TreeNode(left.Value);
+                    queue.Enqueue(node.left);
+                }
+                if (right.HasValue)
+                {
+                    node.right = new TreeNode(right.Value);
+                    queue.Enqueue(node.right);
+                }
+            }
+        }
+        return root;
+    }
+}
+
+public class TreeNode 
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) 
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
