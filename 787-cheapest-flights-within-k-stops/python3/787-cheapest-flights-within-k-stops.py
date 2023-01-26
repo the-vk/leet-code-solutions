5dbd68e99c053d8ef769abec6c0ae00b77613557
Andrew Maraev <andrey.maraev@outlook.com>
2023-01-25 22:27:47 -0800

    improve solution to the problem #787

--- 787-cheapest-flights-within-k-stops/python3/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/python3/787-cheapest-flights-within-k-stops.py
@@ -4,18 +4,20 @@ class Solution:
         for f,t,c in flights:
             edges[f].append((t, c))
         cost_t = [sys.maxsize] * n
-        q = [(src, 0, 0, 0)]
-        ans = sys.maxsize
-        while len(q) > 0:
-            origin, stop, cost, route = q.pop()
+        q = [(src, 0)]
+        stops = 0
+        while q and stops <= k:
+            size = len(q)
+            for i in range(size):
+                origin, cost = q.pop(0)
             
-            for to, price in edges[origin]:
-                cost_to = cost + price
+                for to, price in edges[origin]:
+                    cost_to = cost + price
+                    if cost_to > cost_t[to]:
+                        continue
+                    cost_t[to] = cost_to
+                    q.append((to, cost_to))
+            stops += 1
+                    
+        return cost_t[dst] if cost_t[dst] != sys.maxsize else -1
 
-                if to == dst:
-                    ans = min(ans, cost_to)
-                    continue
-                bit_to = 1 << to
-                if ans > cost_to and stop < k and (bit_to & route) == 0:
-                    q.append((to, stop + 1, cost_to, route | bit_to))
-        return ans if ans != sys.maxsize else -1
