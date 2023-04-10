03dd2755f4a243bd9a5773a056c96f8685cc7d53
Andrew Maraev <andrey.maraev@outlook.com>
2023-04-09 21:54:12 -0700

    improve solution to the problem #20

--- 20-valid-parentheses/python3/20-valid-parentheses.py
+++ 20-valid-parentheses/python3/20-valid-parentheses.py
@@ -1,12 +1,6 @@
-import time
-from typing import List
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        brackets = {}
-        brackets['('] = ')'
-        brackets['['] = ']'
-        brackets['{'] = '}'
+        brackets = {'(': ')', '[': ']', '{': '}'}
         stack = []
         for b in s:
             if b in brackets:
@@ -14,18 +8,3 @@ class Solution:
             elif len(stack) == 0 or b != stack.pop():
                 return False
         return len(stack) == 0
-            
-                
-
-def test(expected, *input):
-    start = time.time_ns()
-    try:
-        solution = Solution()
-        actual = solution.isValid(*input)
-        if actual != expected:
-            print(f"actual value '{actual}' is not equal to expected value '{expected}'")
-    finally:
-        elapsed = (time.time_ns() - start) / (10 ** 9)
-        print(f"elapsed: {elapsed} secs")
-
-test(True, "{[]}")
