8b79df87e0dbad8bc29a3e24abd36b7dcc5ebd7c
Andrew Maraev <andrey.maraev@outlook.com>
2023-11-06 19:06:13 -0800

    update solution to the problem #933

--- 933-number-of-recent-calls/python3/933-number-of-recent-calls.py
+++ 933-number-of-recent-calls/python3/933-number-of-recent-calls.py
@@ -3,16 +3,10 @@ import math
 class RecentCounter:
 
     def __init__(self):
-      self.queue = []
+      self.queue = deque()
         
     def ping(self, t: int) -> int:
       self.queue.append(t)
-      l, r = 0, len(self.queue) - 1
-      while l < r:
-        m = math.floor((l+r)/2)
-        if self.queue[-1] - self.queue[m] <= 3000:
-          r = m
-        else:
-          l = m + 1
-      self.queue = self.queue[l:]
+      while self.queue[0] < t - 3000:
+        self.queue.popleft()
       return len(self.queue)
