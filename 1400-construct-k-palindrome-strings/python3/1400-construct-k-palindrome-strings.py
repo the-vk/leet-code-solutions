fe62a3ebf99bae31395127e02fa1cb3ee9fc8321
Andrew Maraev <andrey.maraev@outlook.com>
2025-01-10 21:35:45 -0800

    improve solution for the problem #1400

--- 1400-construct-k-palindrome-strings/python3/1400-construct-k-palindrome-strings.py
+++ 1400-construct-k-palindrome-strings/python3/1400-construct-k-palindrome-strings.py
@@ -1,15 +1,5 @@
+from collections import Counter
+
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        if k > len(s):
-            return False
-        ctr = {}
-        for v in s:
-            if v in ctr:
-                ctr[v] += 1
-            else:
-                ctr[v] = 1
-        odd_count = 0
-        for _, v in ctr.items():
-            if v % 2 == 1:
-                odd_count += 1
-        return odd_count == 0 or odd_count <= k
+        return not k>len(s) and sum(v&1 for v in Counter(s).values())<=k
