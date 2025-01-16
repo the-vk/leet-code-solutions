32c4bafd8041b0c5bb2a79ca44b0ab760253699f
Andrew Maraev <andrey.maraev@outlook.com>
2025-01-15 22:35:03 -0800

    add solution to the problem #2425

---
+++ 2425-bitwise-xor-of-all-pairings/python3/2425-bitwise-xor-of-all-pairings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+        n1 = len(nums1)
+        n2 = len(nums2)
+
+        ans = 0
+        if n1 % 2 == 1:
+            for i in nums2:
+                ans ^= i
+        if n2 % 2 == 1:
+            for i in nums1:
+                ans ^= i
+        return ans
+        
