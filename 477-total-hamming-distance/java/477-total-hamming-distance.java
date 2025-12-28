d8e2448cbcabb7926c0c1aa984f216024ef11e2d
Andrew Maraev <andrey.maraev@outlook.com>
2025-12-28 15:54:39 -0800

    add solution to the problem #477

--- 477-total-hamming-distance/java/477-total-hamming-distance.java
+++ 477-total-hamming-distance/java/477-total-hamming-distance.java
@@ -1,12 +1,15 @@
 class Solution {
   public int totalHammingDistance(int[] nums) {
     var ans = 0;
-    for (var i = 0; i < nums.length; ++i) {
-      for (var j = i + 1; j < nums.length; ++j) {
-        ans += Integer.bitCount(nums[i] ^ nums[j]);
+    var n = nums.length;
+    for (var j = 0; j < 32; ++j) {
+      var bitCount = 0;
+      for (var i = 0; i < nums.length; ++i) {
+        bitCount += (nums[i] >> j) & 1;
       }
+      
+      ans += bitCount * (n - bitCount);
     }
     return ans;
   }
 }
-
