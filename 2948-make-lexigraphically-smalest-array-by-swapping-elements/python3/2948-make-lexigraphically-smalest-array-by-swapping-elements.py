6c9e9fa2d6085ba66aec2259a3fa873affafaf88
Andrew Maraev <the_vk@thevk.net>
2026-04-01 21:11:03 -0700

    add solution to the problem 2948

--- 2948-make-lexigraphically-smalest-array-by-swapping-elements/python3/2948-make-lexigraphically-smalest-array-by-swapping-elements.py
+++ 2948-make-lexigraphically-smalest-array-by-swapping-elements/python3/2948-make-lexigraphically-smalest-array-by-swapping-elements.py
@@ -1,25 +1,24 @@
 class Solution:
-    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        from collections import deque, defaultdict
+  def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+    from collections import defaultdict, deque
 
-        sorted_nums = sorted(nums)
-        levels = defaultdict(deque)
-        level_map = {}
+    srt = sorted(nums)
+    levels = defaultdict(deque)
+    level_map = {}
+    
+    level = 0
+    levels[level].append(srt[0])
+    level_map[srt[0]] = level
 
-        current_level = 0
-        levels[current_level].append(sorted_nums[0])
-        level_map[sorted_nums[0]] = current_level
+    for i in range(1, len(srt)):
+      v = srt[i]
+      if v - levels[level][-1] > limit:
+        level += 1
+      levels[level].append(v)
+      level_map[v] = level
 
-        for i in range(1, len(sorted_nums)):
-            if sorted_nums[i] - levels[current_level][-1] <= limit:
-                levels[current_level].append(sorted_nums[i])
-            else:
-                current_level += 1
-                levels[current_level].append(sorted_nums[i])
-            level_map[sorted_nums[i]] = current_level
+    for i in range(len(nums)):
+      level = level_map[nums[i]]
+      nums[i] = levels[level].popleft()
 
-        for i in range(len(nums)):
-            element_level = level_map[nums[i]]
-            nums[i] = levels[element_level].popleft()
-
-        return nums
+    return nums
